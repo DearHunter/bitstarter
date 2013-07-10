395ca00ae74fcdc0a3802d29f9126354b2736cea
Ubuntu <ubuntu@ip-172-31-25-87.us-west-2.compute.internal>
2013-07-10 14:43:32 +0000

    Added fs require

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var ind = fs.readFileSync('index.html');
 var buffer = new Buffer(ind);
 
 app.get('/', function(request, response) {
-  response.send(buffer);
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
