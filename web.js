1b5ef49ee3b204dbf0ef6c5213ced46313dbb2ca
Ubuntu <ubuntu@ip-172-31-25-87.us-west-2.compute.internal>
2013-07-10 14:27:02 +0000

    Tried with buffer

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var ind = fs.readFileSync('index.html')
+var ind = fs.readFileSync('index.html');
+var buffer = new Buffer(ind);
+
 app.get('/', function(request, response) {
-  response.send(ind);
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
