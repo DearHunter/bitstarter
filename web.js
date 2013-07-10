65cd5a004f545356690e33367c139eba9cf44920
Ubuntu <ubuntu@ip-172-31-25-87.us-west-2.compute.internal>
2013-07-10 14:24:55 +0000

    Modified web.js, but w/o buffer...

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var ind = fs.readFileSync('index.html')
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(ind);
 });
 
 var port = process.env.PORT || 5000;
